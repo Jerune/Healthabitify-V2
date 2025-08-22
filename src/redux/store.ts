a5a9bbd2543816ced8e6706ca1115060f7362fed
Jeroen van de Sanden <vandesanden@hotmail.com>
2025-08-22 14:32:05 +0200

    build: make sure store is only visible on dev server

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,16 +5,26 @@ import metricsReducer from './reducers/metricsReducer';
 import usersReducer from './reducers/usersReducer';
 import utilsReducer from './reducers/utilsReducer';
 
-const store = configureStore({
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const store = configureStore({
   reducer: {
     user: usersReducer,
     metrics: metricsReducer,
-    utils: utilsReducer,
     averages: averagesReducer,
+    utils: utilsReducer,
   },
+  devTools: isDevelopment,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: isDevelopment,
+      immutableCheck: isDevelopment,
+    }),
 });
 
-store.subscribe(() => console.log('Redux State:', store.getState()));
+if (isDevelopment) {
+  store.subscribe(() => console.log('Redux State:', store.getState()));
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
