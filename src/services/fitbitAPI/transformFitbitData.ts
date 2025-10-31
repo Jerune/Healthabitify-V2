a699b8408b2a2a67f98126fc0508c0fb6046ff8c
Jeroen van de Sanden <vandesanden@hotmail.com>
2025-10-31 12:37:27 +0100

    feat: add new heart rate datapoints and vo2max to transform function

--- src/services/fitbitAPI/transformFitbitData.ts
+++ src/services/fitbitAPI/transformFitbitData.ts
@@ -3,6 +3,8 @@ import {
   FitbitActivityData,
   FitbitData,
   FitbitRawData,
+  HeartRateZoneData,
+  Vo2maxData,
 } from '../../types';
 import { getDateTimeDataForDatapoints } from '../../utils/getDateTimeData';
 import matchServiceResourcesWithMetricNames from '../matchResources';
@@ -28,8 +30,12 @@ export default function transformFitbitData(
     datapointsCollection[resourceNameFromAPI].forEach(
       (datapoint: FitbitData) => {
         const date = datapoint.dateTime;
+
+        // Manage all datapoints from activities summary
+        // Except heartrate zones and vo2max values
         if (
           metric !== 'heartrate-zones' &&
+          metric !== 'cardioscore' &&
           typeof datapoint.value === 'string'
         ) {
           const { value } = datapoint;
@@ -47,30 +53,53 @@ export default function transformFitbitData(
           });
         }
 
+        // Get lowest value of cardioscore (Vo2max)
+        if (metric === 'vo2-max' && typeof datapoint.value !== 'string') {
+          const { value } = datapoint;
+          const { vo2Max } = value as Vo2maxData;
+          const lowestValue = vo2Max.split('-')[0];
+          const { month, weekNumber, year } =
+            getDateTimeDataForDatapoints(date);
+          newDataPoints.push({
+            userId: 'nbkxUOC66VVE7CbqhloaTQJKiRH3',
+            value: lowestValue,
+            date,
+            source,
+            metric,
+            weekNumber,
+            month,
+            year,
+          });
+        }
+
+        // Get heartrate zones data
         if (
           metric === 'heartrate-zones' &&
           typeof datapoint.value !== 'string'
         ) {
-          datapoint.value.heartRateZones.forEach(heartRateZone => {
-            if (heartRateZone.name !== 'Out of Range') {
-              const metricName = `${heartRateZone.name
-                .split(' ')
-                .join('-')
-                .toLowerCase()}-zone`;
-              const value = heartRateZone.minutes;
-              const { month, weekNumber, year } =
-                getDateTimeDataForDatapoints(date);
-              newDataPoints.push({
-                userId: 'nbkxUOC66VVE7CbqhloaTQJKiRH3',
-                value,
-                date,
-                source,
-                metric: metricName,
-                weekNumber,
-                month,
-                year,
-              });
-            }
+          const metricNames = {
+            activeZoneMinutes: 'active-zone-minutes',
+            fatBurnActiveZoneMinutes: 'fat-burn-zone',
+            cardioActiveZoneMinutes: 'cardio-zone',
+            peakActiveZoneMinutes: 'peak-zone',
+          };
+
+          const { value } = datapoint;
+          const heartRateZones = value as HeartRateZoneData;
+          Object.entries(heartRateZones).forEach(heartRateZone => {
+            const [name, minutes] = heartRateZone;
+            const { month, weekNumber, year } =
+              getDateTimeDataForDatapoints(date);
+            newDataPoints.push({
+              userId: 'nbkxUOC66VVE7CbqhloaTQJKiRH3',
+              value: minutes,
+              date,
+              source,
+              metric: metricNames[name as keyof typeof metricNames],
+              weekNumber,
+              month,
+              year,
+            });
           });
         }
       }
